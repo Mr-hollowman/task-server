d89c0af878a68a3849f6d67fcf673b05d5b7db59
mr-hollowman <selvamani825@gmail.com>
2023-04-24 10:29:45 +0530

    restriction added for req origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://my-freeelancer.web.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
